e4756e1088ac4906feb9a3b24bd0cae25598003a
JaydenECampbell <101jayden.campbell@gmail.com>
2026-03-02 20:13:22 +0000

    yippee

--- src/osshell.cpp
+++ src/osshell.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <filesystem>
 #include <unistd.h>
+#include <sys/wait.h>
 
 bool fileExecutableExists(std::string file_path);
 void splitString(std::string text, char d, std::vector<std::string>& result);
@@ -30,8 +31,102 @@ int main (int argc, char **argv)
     // Welcome message
     printf("Welcome to OSShell! Please enter your commands ('exit' to quit).\n");
 
+    /*
+    Provide a prompt for a user to enter a command (osshell> )
+
+    Search the directories in the PATH environment variable and find the first executable with the name of the entered command
+        If one is found, spawn a new process to run that executable, and wait for its completion (i.e. fork() and exec())
+        If not found, print the following error message:
+        "<command>: Error command not found"
+    Just hitting enter without typing a command should not print an error
+    After a command executes (or no command is found and error printed, or user simply hit enter), the shell interface should prompt the user for the next command
+
+    */
     // Repeat:
     //  Print prompt for user input: "osshell> " (no newline)
+    while (true)
+    {
+        std::cout << "osshell> ";
+        std::getline(std::cin, user_command);
+        if (user_command == "")
+        {
+            continue;
+        }
+
+        history.push_back(user_command);  // Store command in history
+
+        if (user_command == "exit")
+        {
+            break;
+        }
+        
+        else if (user_command == "history")
+        {
+            int i;
+            for (i = 0; i < history.size(); i++)
+            {
+                printf("%2d: %s\n", i, history[i].c_str());
+            }
+            printf("------\n");
+        }
+
+        else if (user_command != "")
+        {
+            splitString(user_command, ' ', command_list);
+            if (command_list.size() == 0)
+            {
+                continue;
+            }
+
+            std::string executable_path = "";   // Searches path for executable with name of command user entered, and stores full path to that executable here if found
+            std::string command_name = command_list[0];
+
+            if (command_name[0] == '.' || command_name[0] == '/')
+            {
+                if (fileExecutableExists(command_name))
+                {
+                    executable_path = command_name;
+                }
+            }
+
+            else
+            {
+                for (int i = 0; i < os_path_list.size(); i++)
+                {
+                    std::string full_path = os_path_list[i] + "/" + command_name;
+
+                    if (fileExecutableExists(full_path))
+                    {
+                        executable_path = full_path;
+                        break;
+                    }
+                }
+            }
+
+            // If not found → error
+            if (executable_path == "")
+            {
+                std::cout << command_name << "<command>: Error command not found" << std::endl;
+                continue;
+            }
+
+            command_list[0] = executable_path;  // Replaces command name with full path to executable for execv() function
+            vectorOfStringsToArrayOfCharArrays(command_list, &command_list_exec);   // Converts to char** for execv() function
+            pid_t pid = fork(); // Forks process
+            if (pid == 0)
+            {
+                execv(executable_path.c_str(), command_list_exec);  // Executes command in child process
+                perror("execv failed"); // If execv returns, there was an error
+                exit(1);
+            }
+
+            else if (pid > 0)
+            {
+                int status;
+                waitpid(pid, &status, 0); // Waits for child process to finish
+            }
+        }
+    }
     //  Get user input for next command
     //  If command is `exit` exit loop / quit program
     //  If command is `history` print previous N commands
@@ -39,11 +134,13 @@ int main (int argc, char **argv)
     //   If yes, execute it
     //   If no, print error statement: "<command_name>: Error command not found" (do include newline)
 
-
+    
 
     /************************************************************************************
      *   Example code - remove in actual program                                        *
      ************************************************************************************/
+     
+    /*
     // Shows how to loop over the directories in the PATH environment variable
     int i;
     for (i = 0; i < os_path_list.size(); i++)
@@ -81,6 +178,8 @@ int main (int argc, char **argv)
     // free memory for `command_list_exec`
     freeArrayOfCharArrays(command_list_exec, command_list.size() + 1);
     printf("------\n");
+
+    
     /************************************************************************************
      *   End example code                                                               *
      ************************************************************************************/
@@ -96,8 +195,10 @@ int main (int argc, char **argv)
 bool fileExecutableExists(std::string file_path)
 {
     bool exists = false;
-    // check if `file_path` exists
-    // if so, ensure it is not a directory and that it has executable permissions
+    if (access(file_path.c_str(), F_OK) == 0 && access(file_path.c_str(), X_OK) == 0)
+    {
+        exists = true;
+    }
 
     return exists;
 }
@@ -196,3 +297,5 @@ void freeArrayOfCharArrays(char **array, size_t array_length)
     }
     delete[] array;
 }
+
+
